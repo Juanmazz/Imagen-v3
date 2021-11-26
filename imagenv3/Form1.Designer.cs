f0fc12b7c3d8c59e098e30338f972098e030e89a
juanmazz <jaunmazziad@gmail.com>
2021-11-26 17:11:59 -0300

    Imagen v3

---
+++ imagenv3/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace imagenv3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.piccentral = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccentral)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(721, 12);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(140, 122);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 3;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(721, 140);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(140, 122);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 4;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(721, 268);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(140, 122);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 5;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic4
+            // 
+            this.pic4.Location = new System.Drawing.Point(721, 396);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(140, 122);
+            this.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic4.TabIndex = 6;
+            this.pic4.TabStop = false;
+            this.pic4.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic5
+            // 
+            this.pic5.Location = new System.Drawing.Point(721, 524);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(140, 122);
+            this.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic5.TabIndex = 7;
+            this.pic5.TabStop = false;
+            this.pic5.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // piccentral
+            // 
+            this.piccentral.Location = new System.Drawing.Point(95, 83);
+            this.piccentral.Name = "piccentral";
+            this.piccentral.Size = new System.Drawing.Size(506, 476);
+            this.piccentral.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piccentral.TabIndex = 8;
+            this.piccentral.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 661);
+            this.Controls.Add(this.piccentral);
+            this.Controls.Add(this.pic5);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccentral)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox piccentral;
+    }
+}
+
---
+++ imagenv3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imagenv3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+
+            PictureBox aux = (PictureBox)sender;
+
+            piccentral.Image = aux.Image;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string dir = "C:\\Users\\JUAN PABLO\\Desktop\\Imagenes\\";
+
+            pic1.Image = Image.FromFile(dir+"cos1.jpg");
+            pic2.Image = Image.FromFile(dir+"cos2.jpg");
+            pic3.Image = Image.FromFile(dir+"cos3.jpg");
+            pic4.Image = Image.FromFile(dir+"cos4.jpg");
+            pic5.Image = Image.FromFile(dir+"cos5.jpg");
+
+        }
+    }
+}
